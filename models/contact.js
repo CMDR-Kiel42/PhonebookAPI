8951d91576520e610ee230a278ca468fa28833bc
Antony Jeunemaitre <klideyla@gmail.com>
2021-12-02 21:33:04 +0100

    Add phone validator for mongoose

--- models/contact.js
+++ models/contact.js
@@ -1,12 +1,32 @@
 var mongoose = require('mongoose');
 
+
+const PhoneSchema = new mongoose.Schema({
+    home: {type: String},
+    mobile: {type: String},
+    work: {type: String},
+    other: {type: String},
+});
+
+function phoneValidator(phone) {
+    var ret = (phone && 
+        (typeof phone.home != 'undefined' && phone.home.length > 2)     ||
+        (typeof phone.mobile != 'undefined' && phone.mobile.length > 2) ||
+        (typeof phone.work != 'undefined' && phone.work.length > 2)     ||
+        (typeof phone.other != 'undefined' && phone.other.length > 2)
+    );
+    return ret;
+};
+
 var ContactSchema = new mongoose.Schema({
     name: { type: String, required: true },
     phone: {
-		home: {type: String},
-		mobile: {type: String},
-		work: {type: String},
-		other: {type: String},
+        type: PhoneSchema,
+        required: [true, 'At least one phone number should be given'],
+        validate: {
+            validator: phoneValidator,
+            message: props => 'At least one phone number should be given'
+        }
     },
     email: { type: String },
     address: { 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 const router = express.Router();
