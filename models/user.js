a62997bb7d534bd4a6d565062dacf43b2f829b90
Antony Jeunemaitre <klideyla@gmail.com>
2021-12-02 22:07:37 +0100

    Hash user passwords before saving them

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	username: {
@@ -11,5 +12,30 @@ var UserSchema = new mongoose.Schema({
 		required: true
 	}
 });
+
+UserSchema.pre('save', function(callback) {
+	const user = this;
+
+	bcrypt.genSalt(5, function(err, salt) {
+		if (err) {
+			return callback(err);
+		}
+		else {
+			user.password = bcrypt.hashSync(user.password, salt);
+			callback();
+		}
+	});
+});
+
+UserSchema.methods.verifyPassword = function(password, callback) {
+	bcrypt.compare(password, this.password, function (err, isMatch) {
+		if (err) {
+			return callback(err);
+		}
+		else {
+			callback(null, isMatch);
+		}
+	})
+};
 
 module.exports = mongoose.model('User', UserSchema);
